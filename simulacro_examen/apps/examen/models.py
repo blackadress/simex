10991ad2558578a755e2a635bd566bedcde72aa0
blackadress <blackadress.01@gmail.com>
2020-10-12 19:26:16 -0500

    inicial

---
+++ simulacro_examen/apps/examen/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+
+# Create your models here.
+
+class Universidad(models.Model):
+    nombre = models.CharField(max_length=200, null=False)
+
+    class Meta:
+        verbose_name = "Universidad"
+        verbose_name_plural = "Universidades"
+
+class Facultad(models.Model):
+    nombre = models.CharField(max_length=200, null=False)
+
+    universidad = models.ForeignKey(Universidad, on_delete=models.SET_NULL, null=True)
+    class Meta:
+        verbose_name = "Facultad"
+        verbose_name_plural = "Facultades"
+
+class EscuelaProfesional(models.Model):
+    nombre = models.CharField(max_length=200, null=False)
+
+    facultad = models.ForeignKey(Facultad, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Escuela Profesional"
+        verbose_name_plural = "Escuelas Profesionales"
+
+class Examen(models.Model):
+    nombre_examen = models.CharField(max_length=200, null=False)
+    tipo_examen = models.CharField(max_length=200, null=False)
+    duracion_minutos = models.IntegerField(default=60)
+    nota_maxima = models.DecimalField(max_digits=4, decimal_places=3)
+    puntaje_maximo = models.DecimalField(max_digits=4, decimal_places=3)
+
+    universidad = models.ForeignKey(Universidad, on_delete=models.SET_NULL, null=True)
+    # examen_pregunta = models.ForeignKey("ExamenPregunta", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Examen"
+        verbose_name_plural = "Examenes"
+
+class ExamenPregunta(models.Model):
+    examen = models.ForeignKey("Examen", on_delete=models.SET_NULL, null=True)
+    pregunta = models.ForeignKey("Pregunta", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Examen Pregunta"
+        verbose_name_plural = "Examenes Preguntas"
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200, null=False)
+    cantidad_preguntas = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+
+class TipoPuntuacion(models.Model):
+    favor = models.DecimalField(max_digits=3, decimal_places=3)
+    contra = models.DecimalField(max_digits=3, decimal_places=3)
+    sin_responder = models.DecimalField(max_digits=3, decimal_places=3)
+
+    curso = models.ForeignKey("Curso", on_delete=models.SET_NULL, null=True)
+    examen = models.ForeignKey("Examen", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Tipo Puntuacion"
+        verbose_name_plural = "Tipos de Puntuacion"
+
+class Pregunta(models.Model):
+    nombre = models.CharField(max_length=200, null=False)
+    contenido = models.CharField(max_length=300, null=False)
+
+    curso = models.ForeignKey("Curso", on_delete=models.SET_NULL, null=True)
+    # examen_pregunta = models.ForeignKey("ExamenPregunta", on_delete=models.SET_NULL, null=True)
+    class Meta:
+        verbose_name = "Pregunta"
+        verbose_name_plural = "Contenido"
+
+class Alternativa(models.Model):
+    alternativa = models.CharField(max_length=200, null=False)
+    clave = models.CharField(max_length=200, null=False)
+
+    pregunta = models.ForeignKey("Pregunta", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Alternativa"
+        verbose_name_plural = "Alternativas"
+
+class ResultadoExamen(models.Model):
+    duracion_segundos = models.IntegerField()
+    nota_obtenida = models.DecimalField(max_digits=4, decimal_places=3)
+    puntaje_obtenido = models.DecimalField(max_digits=4, decimal_places=3)
+
+    examen = models.ForeignField("Examen", on_delete=models.SET_NULL, null=True)
+    alumno = models.ForeignField("Alumno", on_delete=models.SET_NULL, null=True)
+    class Meta:
+        verbose_name = "Resultado Examen"
+        verbose_name_plural = "Resultados Examenes"
+
---
+++ simulacro_examen/apps/usuario/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    TIPO_USUARIO = (
+        ("alumno", "alumno"),
+        ("docente", "docente"),
+        ("administrador", "administrador"),
+    )
+    nombres = models.CharField(max_length=200, null=False)
+    apellido_paterno = models.CharField(max_length=200, null=False)
+    apellido_materno = models.CharField(max_length=200, null=False)
+    correo_electronico = models.CharField(max_length=200, null=False)
+    celular = models.CharField(max_length=9, null=True, blank=True, default="000000000")
+    tipo_usuario = models.CharField(blank=False, null=False, default="alumno", choices=TIPO_USUARIO)
+
+    usuario = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+class Docente(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Docente"
+        verbose_name_plural = "Docentes"
+
+class Alumno(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Alumno"
+        verbose_name_plural = "Alumnos"
+
