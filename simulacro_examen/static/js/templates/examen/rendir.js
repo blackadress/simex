aa750cc8e6995f334b51ad945c555197be2a776f
blackadress <blackadress.01@gmail.com>
2020-12-08 22:13:49 -0500

    raloj 40%

--- simulacro_examen/static/js/templates/examen/rendir.js
+++ simulacro_examen/static/js/templates/examen/rendir.js
@@ -11,3 +11,40 @@ const send_examen = () => {
   
 
 }
+
+const parse_hora = () => {
+  const inicio = document.querySelector('#inicio_examen').innerHTML
+  const final = document.querySelector('#entrega_examen').innerHTML
+
+  const fecha_inicio = fecha_texto_a_fecha(inicio)
+  const fecha_final = fecha_texto_a_fecha(final)
+
+  const today = new Date()
+  // si fecha today es mayor a fecha entrega, no hay reloj
+  // si fecha today es menor hay reloj
+  // TODO hacer reloj
+}
+
+const fecha_texto_a_fecha = (fecha) => {
+  let separado = fecha.split(',')
+  let re_day = /(\d*)/
+  let re_month = /\: ([A-Za-z]{3})/
+  let re_year = /(\d{4})/
+
+  let day = re_day.exec(separado[0])[1]
+  let month = re_month.exec(separado[0])[1]
+  let year = re_year.exec(separado[1])[1]
+
+  let re_hour = /(\d*)/
+  let re_minutes = /:(\d*)/
+
+  let hour = re_hour.exec(separado[2])[1]
+  let minutes = re_minutes.exec(separado[2])[1]
+
+  let ampm = separado[2].split(' ').pop()
+  if (ampm == "p.m.") {
+    hour += 12
+  }
+
+  return new Date(`${year} ${month} ${day} ${hour}:${minutes}`)
+}
